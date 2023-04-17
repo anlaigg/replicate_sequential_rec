36f0e53bfc49c8919dca61d905e33fa990129bd8
Li-fAngyU <972117977@qq.com>
2023-04-17 14:27:21 +0800

    first change

--- README.md
+++ README.md
@@ -1,70 +1,24 @@
+Our code is forked from https://github.com/aHuiWang/CIKM2020-S3Rec
 
-Code for our CIKM 2020 Paper ["**S3-Rec: Self-Supervised Learning for Sequential
- Recommendation with Mutual Information Maximization"**](https://arxiv.org/pdf/2008.07873.pdf)
+We provide the replication code for all experimental results. The specific steps are as follows:
 
-## Overview
+# Replicating Results of Enhanced Loss Function on CIKM2020-S3Rec
 ![avatar](model.PNG)
 
-## Reproduce
-Please check the ./reproduce/ directory.
-
-## Results
-Performance comparison of different methods on six datasets. The best performance and the second best performance
-methods are denoted in bold and underlined fonts respectively.
-
-In the PAPER, we pair the ground-truth item 
-with **99 randomly sampled negative items** that the user
-has not interacted with, and report the results of 
-HR@{1, 5, 10}, NDCG@{5, 10} and MRR. The used test files are name as 
-```
-data-name_sample.txt
-```
-The results are shown in the following pic.
-![avatar](sample_99.PNG)
-
-
-We alse rank the ground-truth item with **all the items**.
-We omit the FM and AutoInt because they need 
-enumerate all user-item pairs, which take a very long time. 
-
-The results are shown in the following pic.
-
-
-![avatar](all_rank.PNG)
-
-### requirements
-```shell script
+## Preparation 
+```python
 pip install -r requirements.txt
 ```
 
-## data format
-```shell script
-data preprocess
-./data/data_process.py
+## Replicating Results
+![introduction](introduction.svg)
+We provide a simple script file for one-step reproduction of the experimental results presented in the introduction section.
 
-generate negative items for testing
-./data/generate_test.py
-
-
-data-name.txt
-one user per line
-user_1 item_1 item_2 ...
-user_2 item_1 item_2 ...
-
-data-name_sample.txt
-one user per line
-user_1 neg_item_1 neg_item_2 ...
-user_2 neg_item_1 neg_item_2 ...
-
-data-name_item2attributes.json
-{item_1:[attr, ...], item_2:[attr, ...], ... }
+```python
+python run_introduction_experiment.py
 ```
 
-## pretrain
-```shell script
-python run_pretrain.py \
---data_name data_name
-```
+Result save at `.output/introduction/All_result.txt`, and the training log save at  `.output/introduction/.*`
 
 ## finetune
 We support two evaluation methods. For more details, please check the ./reproduce directory.
--- trainers.py
+++ trainers.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2020/3/30 11:06
-# @Author  : Hui Wang
-
 import numpy as np
 import tqdm
 import random
@@ -37,11 +33,12 @@ class Trainer:
 
         print("Total Parameters:", sum([p.nelement() for p in self.model.parameters()]))
         self.criterion = nn.BCELoss()
-    def train(self, epoch):
-        self.iteration(epoch, self.train_dataloader)
 
-    def valid(self, epoch, full_sort=False):
-        return self.iteration(epoch, self.eval_dataloader, full_sort, train=False)
+    def train(self, epoch, verbose = False):
+        return self.iteration(epoch, self.train_dataloader, verbose=verbose)
+
+    def valid(self, epoch, full_sort=False, verbose = False):
+        return self.iteration(epoch, self.eval_dataloader, full_sort, train=False, verbose = verbose)
 
     def test(self, epoch, full_sort=False):
         return self.iteration(epoch, self.test_dataloader, full_sort, train=False)
@@ -49,7 +46,7 @@ class Trainer:
     def iteration(self, epoch, dataloader, full_sort=False, train=True):
         raise NotImplementedError
 
-    def get_sample_scores(self, epoch, pred_list):
+    def get_sample_scores(self, epoch, pred_list, verbose = False):
         pred_list = (-pred_list).argsort().argsort()[:, 0]
         HIT_1, NDCG_1, MRR = get_metric(pred_list, 1)
         HIT_5, NDCG_5, MRR = get_metric(pred_list, 5)
@@ -61,12 +58,13 @@ class Trainer:
             "HIT@10": '{:.4f}'.format(HIT_10), "NDCG@10": '{:.4f}'.format(NDCG_10),
             "MRR": '{:.4f}'.format(MRR),
         }
-        print(post_fix)
+        if verbose:
+            print(post_fix)
         with open(self.args.log_file, 'a') as f:
             f.write(str(post_fix) + '\n')
         return [HIT_1, NDCG_1, HIT_5, NDCG_5, HIT_10, NDCG_10, MRR], str(post_fix)
 
-    def get_full_sort_score(self, epoch, answers, pred_list):
+    def get_full_sort_score(self, epoch, answers, pred_list, verbose = False):
         recall, ndcg = [], []
         for k in [5, 10, 15, 20]:
             recall.append(recall_at_k(answers, pred_list, k))
@@ -77,9 +75,13 @@ class Trainer:
             "HIT@10": '{:.4f}'.format(recall[1]), "NDCG@10": '{:.4f}'.format(ndcg[1]),
             "HIT@20": '{:.4f}'.format(recall[3]), "NDCG@20": '{:.4f}'.format(ndcg[3])
         }
-        print(post_fix)
+        if verbose:
+            print(post_fix)
+            self.args.writer.add_scalar('HIT@10',recall[1], epoch)
+            self.args.writer.add_scalar('NDCG@10',ndcg[1], epoch)
+
         with open(self.args.log_file, 'a') as f:
-            f.write(str(post_fix) + '\n')
+            f.write(str(post_fix) + '\n')        
         return [recall[0], ndcg[0], recall[1], ndcg[1], recall[3], ndcg[3]], str(post_fix)
 
     def save(self, file_name):
@@ -89,23 +91,123 @@ class Trainer:
     def load(self, file_name):
         self.model.load_state_dict(torch.load(file_name))
 
-    def cross_entropy(self, seq_out, pos_ids, neg_ids):
+    def bce_loss(self, seq_out, pos_ids, neg_ids, sample_num = 0):
         # [batch seq_len hidden_size]
-        pos_emb = self.model.item_embeddings(pos_ids)
-        neg_emb = self.model.item_embeddings(neg_ids)
-        # [batch*seq_len hidden_size]
-        pos = pos_emb.view(-1, pos_emb.size(2))
-        neg = neg_emb.view(-1, neg_emb.size(2))
-        seq_emb = seq_out.view(-1, self.args.hidden_size) # [batch*seq_len hidden_size]
-        pos_logits = torch.sum(pos * seq_emb, -1) # [batch*seq_len]
-        neg_logits = torch.sum(neg * seq_emb, -1)
-        istarget = (pos_ids > 0).view(pos_ids.size(0) * self.model.args.max_seq_length).float() # [batch*seq_len]
-        loss = torch.sum(
-            - torch.log(torch.sigmoid(pos_logits) + 1e-24) * istarget -
-            torch.log(1 - torch.sigmoid(neg_logits) + 1e-24) * istarget
-        ) / torch.sum(istarget)
+        batch, seq_len, hidden_size = seq_out.shape
+        if sample_num != 0:
+            sample_ids = np.random.choice(self.args.item_size, (batch, seq_len, sample_num))
+            tmp_targets = np.repeat(np.array(pos_ids.tolist()), sample_num).reshape(batch, seq_len, sample_num)
+            mask = tmp_targets == sample_ids
+            sample_ids[mask] = self.args.mask_id
+            neg_ids = torch.from_numpy(sample_ids).to(pos_ids.device)
 
+            pos_emb = self.model.item_embeddings(pos_ids)
+            neg_emb = self.model.item_embeddings(neg_ids)
+            # [batch*seq_len hidden_size]
+            pos = pos_emb.view(-1, pos_emb.shape[-1])
+            # [batch*seq_len, sample_num, hidden_size]
+            neg = neg_emb.view(-1, sample_num, neg_emb.shape[-1])
+            seq_emb = seq_out.view(-1, self.args.hidden_size) # [batch*seq_len hidden_size]
+            seq_emb_neg = seq_out.unsqueeze(2).repeat(1,1,sample_num,1).view(-1, sample_num, self.args.hidden_size)
+            pos_logits = torch.sum(pos * seq_emb, -1) # [batch*seq_len]
+            neg_logits = torch.sum(neg * seq_emb_neg, -1) #[batch*seq_len, sample_num]
+            istarget = (pos_ids > 0).view(pos_ids.size(0) * self.model.args.max_seq_length).float() # [batch*seq_len]
+            istarget_neg = istarget.unsqueeze(-1).repeat(1, sample_num)
+            #TODO change this similarity
+            loss = torch.sum(
+                - torch.log(torch.sigmoid(pos_logits) + 1e-24) * istarget -
+                (torch.log(1 - torch.sigmoid(neg_logits) + 1e-24) * istarget_neg).mean()
+            ) / torch.sum(istarget)
+            return loss
+        else:
+            pos_emb = self.model.item_embeddings(pos_ids)
+            neg_emb = self.model.item_embeddings(neg_ids)
+            # [batch*seq_len hidden_size]
+            pos = pos_emb.view(-1, pos_emb.shape[-1])
+            # [batch*seq_len*sample_num hidden_size]
+            neg = neg_emb.view(-1, neg_emb.shape[-1])
+            seq_emb = seq_out.view(-1, self.args.hidden_size) # [batch*seq_len hidden_size]
+            pos_logits = torch.sum(pos * seq_emb, -1) # [batch*seq_len]
+            neg_logits = torch.sum(neg * seq_emb, -1)
+            istarget = (pos_ids > 0).view(pos_ids.size(0) * self.model.args.max_seq_length).float() # [batch*seq_len]
+            #TODO change this similarity
+            loss = torch.sum(
+                - torch.log(torch.sigmoid(pos_logits) + 1e-24) * istarget -
+                torch.log(1 - torch.sigmoid(neg_logits) + 1e-24) * istarget
+            ) / torch.sum(istarget)
+            return loss
+
+    def cce_loss(self, seq_out, target_pos):
+        test_item_emb = self.model.item_embeddings.weight
+        pos_mask = (target_pos>0)
+        seq_mask = pos_mask.unsqueeze(-1).repeat(1, 1, self.args.hidden_size)
+        target_pos = target_pos.masked_select(pos_mask)
+        seq_out = seq_out.masked_select(seq_mask).view(-1, self.args.hidden_size)
+        logits = torch.matmul(seq_out, test_item_emb.transpose(0, 1))
+        loss = nn.CrossEntropyLoss()(logits, target_pos)
+        return  loss
+    
+    def bpr_loss(self, seq_out, target_pos, sample_num, use_softmax=False):
+        rating_pred = self.predict_full(seq_out)
+        batch_size = seq_out.shape[0]
+        targets = target_pos[:,-1]   
+        pos = self.model.item_embeddings(targets)
+        pos_logits = torch.sum(pos*seq_out, -1).unsqueeze(-1)
+
+        sample_ids = np.random.choice(self.args.item_size, (batch_size,sample_num)) #, replace = False)
+        tmp_targets = np.repeat(np.array(targets.tolist()), sample_num).reshape(batch_size,sample_num)
+        mask = tmp_targets == sample_ids
+        sample_ids[mask] = self.args.mask_id
+        sample_ids = torch.from_numpy(sample_ids).to(targets.device)
+        neg = self.model.item_embeddings(sample_ids)
+        neg_logits = torch.sum(seq_out.unsqueeze(-1)*neg.transpose(2,1), -1)
+
+        if use_softmax: # BPR-max
+            coeff = torch.softmax(neg_logits, -1)
+            loss = -torch.log(1e-10 + coeff*torch.sigmoid(pos_logits-neg_logits)).mean()
+        else:
+            loss = -torch.log(1e-10 + torch.sigmoid(pos_logits-neg_logits)).mean()
         return loss
+
+    def top1_loss(self, seq_out, target_pos, sample_num, use_softmax=False):
+        rating_pred = self.predict_full(seq_out)
+        batch_size = seq_out.shape[0]
+        targets = target_pos[:,-1]   
+        pos = self.model.item_embeddings(targets)
+        pos_logits = torch.sum(pos*seq_out, -1).unsqueeze(-1)
+
+        sample_ids = np.random.choice(self.args.item_size, (batch_size,sample_num)) #, replace = False)
+        tmp_targets = np.repeat(np.array(targets.tolist()), sample_num).reshape(batch_size,sample_num)
+        mask = tmp_targets == sample_ids
+        sample_ids[mask] = self.args.mask_id
+        sample_ids = torch.from_numpy(sample_ids).to(targets.device)
+        neg = self.model.item_embeddings(sample_ids)
+        neg_logits = torch.sum(seq_out.unsqueeze(-1)*neg.transpose(2,1), -1)
+
+        if use_softmax: # TOP1-max
+            coeff = torch.softmax(neg_logits, -1)
+            loss = (coeff*(torch.sigmoid(neg_logits-pos_logits) + torch.pow(neg_logits, 2))).mean()
+        else:
+            loss = torch.sigmoid(neg_logits-pos_logits).mean() + torch.pow(neg_logits, 2).mean()
+        return loss
+
+    def ce_loss(self, seq_out, target_pos):
+        seq_out = seq_out[:,-1,:]
+        target_pos = target_pos[:,-1]
+        test_item_emb = self.model.item_embeddings.weight
+        logits = torch.matmul(seq_out, test_item_emb.transpose(0, 1))
+        loss = nn.CrossEntropyLoss()(logits, target_pos)
+        return  loss
+
+    def mlm_loss(self, seq_out, target_pos):
+        test_item_emb = self.model.item_embeddings.weight
+        pos_mask = (target_pos>0)
+        seq_mask = pos_mask.unsqueeze(-1).repeat(1, 1, self.args.hidden_size)
+        target_pos = target_pos.masked_select(pos_mask)
+        seq_out = seq_out.masked_select(seq_mask).view(-1, self.args.hidden_size)
+        logits = torch.matmul(seq_out, test_item_emb.transpose(0, 1))
+        loss = nn.CrossEntropyLoss()(logits, target_pos)
+        return  loss
 
     def predict_sample(self, seq_out, test_neg_sample):
         # [batch 100 hidden_size]
@@ -127,6 +229,7 @@ class PretrainTrainer(Trainer):
                  train_dataloader,
                  eval_dataloader,
                  test_dataloader, args):
+        
         super(PretrainTrainer, self).__init__(
             model,
             train_dataloader,
@@ -203,7 +306,7 @@ class FinetuneTrainer(Trainer):
             test_dataloader, args
         )
 
-    def iteration(self, epoch, dataloader, full_sort=False, train=True):
+    def iteration(self, epoch, dataloader, full_sort=False, train=True, verbose = False):
 
         str_code = "train" if train else "test"
 
@@ -223,8 +326,44 @@ class FinetuneTrainer(Trainer):
                 batch = tuple(t.to(self.device) for t in batch)
                 _, input_ids, target_pos, target_neg, _ = batch
                 # Binary cross_entropy
+
+                if self.args.loss_type == 'MLM':
+                    mask = torch.rand(input_ids.size(0), input_ids.size(1)) < self.args.mask_prod
+                    mask = mask.to(target_pos.device)
+                    target_pos = target_pos.masked_fill(~mask, 0)
+                    input_ids = input_ids.masked_fill(mask, 0)                    
+
                 sequence_output = self.model.finetune(input_ids)
-                loss = self.cross_entropy(sequence_output, target_pos, target_neg)
+
+                if self.args.loss_type == 'BCE':
+                    loss = self.bce_loss(sequence_output, target_pos, target_neg, self.args.sample_num)
+                elif self.args.loss_type == 'CCE':
+                    loss = self.cce_loss(sequence_output, target_pos)
+                elif self.args.loss_type == 'BPR':
+                    loss = self.bpr_loss(sequence_output[:, -1, :], 
+                                        target_pos, 
+                                        sample_num = self.args.sample_num)
+                elif self.args.loss_type == 'BPR-max':
+                    loss = self.bpr_loss(sequence_output[:, -1, :], 
+                                        target_pos, 
+                                        sample_num = self.args.sample_num,
+                                        use_softmax = True)
+                elif self.args.loss_type == 'TOP1':
+                    loss = self.top1_loss(sequence_output[:, -1, :], 
+                                        target_pos, 
+                                        sample_num = self.args.sample_num)
+                elif self.args.loss_type == 'TOP1-max':
+                    loss = self.top1_loss(sequence_output[:, -1, :], 
+                                        target_pos, 
+                                        sample_num = self.args.sample_num,
+                                        use_softmax = True)
+                elif self.args.loss_type == 'CE':
+                    loss = self.ce_loss(sequence_output, target_pos)
+                elif self.args.loss_type == 'MLM':
+                    loss = self.mlm_loss(sequence_output, target_pos)
+                #TODO add Lambdarankloss 
+                
+
                 self.optim.zero_grad()
                 loss.backward()
                 self.optim.step()
@@ -234,16 +373,20 @@ class FinetuneTrainer(Trainer):
 
             post_fix = {
                 "epoch": epoch,
-                "rec_avg_loss": '{:.4f}'.format(rec_avg_loss / len(rec_data_iter)),
-                "rec_cur_loss": '{:.4f}'.format(rec_cur_loss),
+                "rec_avg_loss": '{:.8f}'.format(rec_avg_loss / len(rec_data_iter)),
+                "rec_cur_loss": '{:.8f}'.format(rec_cur_loss),
             }
 
-            if (epoch + 1) % self.args.log_freq == 0:
-                print(str(post_fix))
+            if verbose:
+                self.args.writer.add_scalar('loss', rec_avg_loss / len(rec_data_iter), epoch)
 
-            with open(self.args.log_file, 'a') as f:
-                f.write(str(post_fix) + '\n')
+            # if (epoch + 1) % self.args.log_freq == 0:
+            #     print(str(post_fix))
 
+            # with open(self.args.log_file, 'a') as f:
+            #     f.write(str(post_fix) + '\n')
+
+            return post_fix, rec_avg_loss / len(rec_data_iter)
         else:
             self.model.eval()
 
@@ -282,7 +425,7 @@ class FinetuneTrainer(Trainer):
                     else:
                         pred_list = np.append(pred_list, batch_pred_list, axis=0)
                         answer_list = np.append(answer_list, answers.cpu().data.numpy(), axis=0)
-                return self.get_full_sort_score(epoch, answer_list, pred_list)
+                return self.get_full_sort_score(epoch, answer_list, pred_list, verbose)
 
             else:
                 for i, batch in rec_data_iter:
@@ -300,4 +443,71 @@ class FinetuneTrainer(Trainer):
                     else:
                         pred_list = np.append(pred_list, test_logits, axis=0)
 
-                return self.get_sample_scores(epoch, pred_list)
+                return self.get_sample_scores(epoch, pred_list, verbose)
+
+    def get_metric(self, full_sort = False, sample_sort = False, verbose= False, epoch=None):
+        self.model.eval()
+        str_code = 'final'
+        epoch = epoch if epoch is not None else 0
+        rec_data_iter = tqdm.tqdm(enumerate(self.test_dataloader),
+                            desc="Recommendation EP_%s" % (str_code),
+                            total=len(self.test_dataloader),
+                            bar_format="{l_bar}{r_bar}")
+        
+        full_res,sample_res = None,None
+
+        pred_list = None
+        if full_sort:
+            answer_list = None
+            for i, batch in rec_data_iter:
+                # 0. batch_data will be sent into the device(GPU or cpu)
+                batch = tuple(t.to(self.device) for t in batch)
+                user_ids, input_ids, target_pos, target_neg, answers, sample_negs = batch
+                recommend_output = self.model.finetune(input_ids)
+
+                recommend_output = recommend_output[:, -1, :]
+                # 推荐的结果
+
+                rating_pred = self.predict_full(recommend_output)
+
+                rating_pred = rating_pred.cpu().data.numpy().copy()
+                batch_user_index = user_ids.cpu().numpy()
+                rating_pred[self.args.train_matrix[batch_user_index].toarray() > 0] = 0
+                # reference: https://stackoverflow.com/a/23734295, https://stackoverflow.com/a/20104162
+                # argpartition 时间复杂度O(n)  argsort O(nlogn) 只会做
+                # 加负号"-"表示取大的值
+                ind = np.argpartition(rating_pred, -20)[:, -20:]
+                # 根据返回的下标 从对应维度分别取对应的值 得到每行topk的子表
+                arr_ind = rating_pred[np.arange(len(rating_pred))[:, None], ind]
+                # 对子表进行排序 得到从大到小的顺序
+                arr_ind_argsort = np.argsort(arr_ind)[np.arange(len(rating_pred)), ::-1]
+                # 再取一次 从ind中取回 原来的下标
+                batch_pred_list = ind[np.arange(len(rating_pred))[:, None], arr_ind_argsort]
+
+                if i == 0:
+                    pred_list = batch_pred_list
+                    answer_list = answers.cpu().data.numpy()
+                else:
+                    pred_list = np.append(pred_list, batch_pred_list, axis=0)
+                    answer_list = np.append(answer_list, answers.cpu().data.numpy(), axis=0)
+            _, full_res = self.get_full_sort_score(epoch, answer_list, pred_list, verbose)
+
+        if sample_sort:
+            for i, batch in rec_data_iter:
+                # 0. batch_data will be sent into the device(GPU or cpu)
+                batch = tuple(t.to(self.device) for t in batch)
+                user_ids, input_ids, target_pos, target_neg, answers, sample_negs = batch
+                recommend_output = self.model.finetune(input_ids)
+                test_neg_items = torch.cat((answers, sample_negs), -1)
+                recommend_output = recommend_output[:, -1, :]
+
+                test_logits = self.predict_sample(recommend_output, test_neg_items)
+                test_logits = test_logits.cpu().detach().numpy().copy()
+                if i == 0:
+                    pred_list = test_logits
+                else:
+                    pred_list = np.append(pred_list, test_logits, axis=0)
+
+            _, sample_res = self.get_sample_scores(epoch, pred_list, verbose)
+
+        return sample_res, full_res
